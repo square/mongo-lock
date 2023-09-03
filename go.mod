6895d3a101c0805066550867849dfb230423af69
franklin <kevin@franklinkim.de>
2023-09-03 12:03:12 +0200

    feat: autostart mongo docker container to run tests

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.13
 require (
 	github.com/go-test/deep v1.0.1
 	github.com/golang/snappy v0.0.2 // indirect
+	github.com/ory/dockertest/v3 v3.10.0
 	go.mongodb.org/mongo-driver v1.9.1
-	golang.org/x/sync v0.0.0-20201008141435-b3e1573b7520 // indirect
 )
--- lock_test.go
+++ lock_test.go
@@ -6,17 +6,63 @@ import (
 	"context"
 	"crypto/rand"
 	"fmt"
+	"log"
+	"os"
 	"sort"
 	"testing"
 	"time"
 
 	"github.com/go-test/deep"
+	"github.com/ory/dockertest/v3"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	lock "github.com/square/mongo-lock"
 )
+
+var testDb *mongo.Client
+
+func TestMain(m *testing.M) {
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not construct pool: %s", err)
+	}
+
+	// uses pool to try to connect to Docker
+	err = pool.Client.Ping()
+	if err != nil {
+		log.Fatalf("Could not connect to Docker: %s", err)
+	}
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.Run("mongo", "6", []string{})
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
+	if err := pool.Retry(func() error {
+		var err error
+		testDb, err = mongo.Connect(context.Background(), options.Client().ApplyURI(fmt.Sprintf("mongodb://localhost:%s", resource.GetPort("27017/tcp"))))
+		if err != nil {
+			return err
+		}
+		return testDb.Ping(context.Background(), nil)
+	}); err != nil {
+		log.Fatalf("Could not connect to database: %s", err)
+	}
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
 
 func getRandomString() string {
 	n := 5
@@ -27,20 +73,10 @@ func getRandomString() string {
 	return fmt.Sprintf("%X", b)
 }
 
-var testDb *mongo.Client
-
 func setup(t *testing.T) *mongo.Collection {
 	collection := getRandomString()
 
 	var err error
-	if testDb == nil {
-		testDb, err = mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-
 	// Add the required unique index on the 'resource' field.
 	index := mongo.IndexModel{
 		Keys:    bson.M{"resource": 1},
