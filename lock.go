c9f6092fab3607b9fbd8254888463bf35d0026bf
Bostjan Marusic <bostjan@bestbytes.de>
2023-10-02 16:58:45 +0200

    fixed/simplified selector

--- lock.go
+++ lock.go
@@ -688,10 +688,9 @@ func (c *Client) sUnlock(ctx context.Context, resourceName, lockId string) error
 // Specifically this removes lock entries where exclusive.lockId is nil AND shared.count is 0
 func (c *Client) Gc(ctx context.Context) error {
 	// Get all empty locks and remove
-	selector := bson.A{
-		bson.D{{Key: "exclusive.lockId", Value: nil}},
-		bson.D{{Key: "shared.count", Value: 0}},
-	}
+	selector := bson.M{"exclusive.lockId": nil,
+		"shared.count": 0}
+
 	_, err := c.collection.DeleteMany(ctx, selector)
 	if err != nil {
 		return err
